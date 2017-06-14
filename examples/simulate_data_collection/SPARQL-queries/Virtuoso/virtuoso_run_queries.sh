353aceed40bac8873637665d1734e5433d156906
Linh Hoang <lhoang2@illinois.edu>
2017-06-14 11:14:50 -0500

    Add files via upload

---
+++ examples/simulate_data_collection/SPARQL-queries/Virtuoso/virtuoso_run_queries.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+echo ""
+echo "Query 6: What data is output by program block simulate_data_collection.collect_data_set?"
+echo ""
+isql 1111 dba dba errors=stdout ~/DataONE-Prov-Summer-2017/examples/simulate_data_collection/SPARQL-queries/Virtuoso/queries/query6.sparql
+
+echo ""
+echo "Query 7: What program blocks provide input directly to simulate_data_collection.collect_data_set?"
+echo ""
+isql 1111 dba dba errors=stdout ~/DataONE-Prov-Summer-2017/examples/simulate_data_collection/SPARQL-queries/Virtuoso/queries/query7.sparql
+
+echo ""
+echo "Query 8: What programs have input ports that receive data simulate_data_collection[cassette_id]"
+echo ""
+isql 1111 dba dba errors=stdout ~/DataONE-Prov-Summer-2017/examples/simulate_data_collection/SPARQL-queries/Virtuoso/queries/query8.sparql
+
+echo ""
+echo "Query 9: How many ports read data simulate_data_collection[frame_number]?"
+echo ""
+isql 1111 dba dba errors=stdout ~/DataONE-Prov-Summer-2017/examples/simulate_data_collection/SPARQL-queries/Virtuoso/queries/query9.sparql
+
+echo ""
+echo "Query 10: How many data are read by more than port in workflow simulate_data_collection?"
+echo ""
+isql 1111 dba dba errors=stdout ~/DataONE-Prov-Summer-2017/examples/simulate_data_collection/SPARQL-queries/Virtuoso/queries/query10.sparql
+
+echo ""
+echo "Query 10: How many data are read by more than port in workflow simulate_data_collection?"
+echo ""
+isql 1111 dba dba errors=stdout ~/DataONE-Prov-Summer-2017/examples/simulate_data_collection/SPARQL-queries/Virtuoso/queries/query11.sparql
+
+echo ""
+echo "Query 10: How many data are read by more than port in workflow simulate_data_collection?"
+echo ""
+isql 1111 dba dba errors=stdout ~/DataONE-Prov-Summer-2017/examples/simulate_data_collection/SPARQL-queries/Virtuoso/queries/query12.sparql
+
+echo ""
+echo "Query 10: How many data are read by more than port in workflow simulate_data_collection?"
+echo ""
+isql 1111 dba dba errors=stdout ~/DataONE-Prov-Summer-2017/examples/simulate_data_collection/SPARQL-queries/Virtuoso/queries/query13.sparql
+
+echo ""
+echo "Query 10: How many data are read by more than port in workflow simulate_data_collection?"
+echo ""
+isql 1111 dba dba errors=stdout ~/DataONE-Prov-Summer-2017/examples/simulate_data_collection/SPARQL-queries/Virtuoso/queries/query14.sparql
+
+echo ""
+echo "Query 10: How many data are read by more than port in workflow simulate_data_collection?"
+echo ""
+isql 1111 dba dba errors=stdout ~/DataONE-Prov-Summer-2017/examples/simulate_data_collection/SPARQL-queries/Virtuoso/queries/query15.sparql
+
+echo ""
+echo "Query 10: How many data are read by more than port in workflow simulate_data_collection?"
+echo ""
+isql 1111 dba dba errors=stdout ~/DataONE-Prov-Summer-2017/examples/simulate_data_collection/SPARQL-queries/Virtuoso/queries/query16.sparql
+
+echo ""
+echo "Query 10: How many data are read by more than port in workflow simulate_data_collection?"
+echo ""
+isql 1111 dba dba errors=stdout ~/DataONE-Prov-Summer-2017/examples/simulate_data_collection/SPARQL-queries/Virtuoso/queries/query17.sparql
+
+echo ""
+echo "Query 10: How many data are read by more than port in workflow simulate_data_collection?"
+echo ""
+isql 1111 dba dba errors=stdout ~/DataONE-Prov-Summer-2017/examples/simulate_data_collection/SPARQL-queries/Virtuoso/queries/query18.sparql
+
+echo ""
+echo "Query 10: How many data are read by more than port in workflow simulate_data_collection?"
+echo ""
+isql 1111 dba dba errors=stdout ~/DataONE-Prov-Summer-2017/examples/simulate_data_collection/SPARQL-queries/Virtuoso/queries/query19.sparql
+
+echo ""
+echo "Query 10: How many data are read by more than port in workflow simulate_data_collection?"
+echo ""
+isql 1111 dba dba errors=stdout ~/DataONE-Prov-Summer-2017/examples/simulate_data_collection/SPARQL-queries/Virtuoso/queries/query20.sparql
+
+echo ""
+echo "Query 10: How many data are read by more than port in workflow simulate_data_collection?"
+echo ""
+isql 1111 dba dba errors=stdout ~/DataONE-Prov-Summer-2017/examples/simulate_data_collection/SPARQL-queries/Virtuoso/queries/extra_query1.sparql
+
+echo ""
+echo "Query 10: How many data are read by more than port in workflow simulate_data_collection?"
+echo ""
+isql 1111 dba dba errors=stdout ~/DataONE-Prov-Summer-2017/examples/simulate_data_collection/SPARQL-queries/Virtuoso/queries/extra_query2.sparql
